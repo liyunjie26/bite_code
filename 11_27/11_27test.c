9e223ad08d877ae47de8b894bd788201acb1dd33
李云杰 <1284905592@qq.com>
2020-11-27 23:06:48 +0800

    test 11_27  code

---
+++ 11_27/11_27test.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+/*typedef struct{
+	int a;
+	char b;
+	short c;
+	short d;
+}AA_t;
+int main()
+{
+	printf("%d", sizeof(AA_t));
+}*/
+#pragma pack(4)/*编译选项，表示4字节对齐 平台：VS2013。语言：C语言*/
+int main(int argc, char* argv[])
+{
+	struct tagTest1
+	{
+		short a;
+		char d;
+		long b;
+		long c;
+	};
+	struct tagTest2
+	{
+		long b;
+		short c;
+		char d;
+		long a;
+	};
+	struct tagTest3
+	{
+		short c;
+		long b;
+		char d;
+		long a;
+	};
+	struct tagTest1 stT1;
+	struct tagTest2 stT2;
+	struct tagTest3 stT3;
+
+	printf("%d %d %d", sizeof(stT1), sizeof(stT2), sizeof(stT3));
+	return 0;
+}
+#pragma pack()
+
