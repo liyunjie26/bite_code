52d550fd8c935f6fe7c631e776f705b665e85889
李云杰 <1284905592@qq.com>
2020-12-13 23:29:31 +0800

    12_13力扣

---
+++ 12_13/12_13/code.c
@@ -0,0 +1,33 @@
+//输入：1->2->6->3->4->5->6,val=6
+//输出：1->2->3->4->5
+//解：
+#include<stdio.h>
+struct ListNode
+{
+	int val;
+	struct ListNode* next;
+};
+struct ListNode* removeElements(struct ListNode* head, int val)
+{
+	struct ListNode* prev, *cur;//cur当前判断节点，prev后面记录节点
+	cur = head;
+	prev = NULL;
+	while (cur)
+	{
+		if (cur->val == val)
+		{
+			struct ListNode* next = cur->next;
+			free(cur);//释放当前的节点
+			if (prev == NULL)//判断是否为头
+				head = next;
+			else
+				prev->next = next;
+			cur = next;
+		}
+		else
+		{
+			prev = cur;
+			cur = cur->next;
+		}
+	}
+}
